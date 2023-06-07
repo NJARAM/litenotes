4952891ba4c17d12878cfbfa29fc1d6005982cdb
Moses Muchoki <mosesmmuchoki@gmail.com>
2023-06-07 23:46:45 +0300

    Added string limiter helper function

--- app/Http/Controllers/NoteContrroller.php
+++ app/Http/Controllers/NoteContrroller.php
@@ -14,7 +14,7 @@ class NoteContrroller extends Controller
     public function index()
     {
        
-       $notes=Note::where('user_id',Auth::id())->latest('updated_at')->paginate(1);
+       $notes=Note::where('user_id',Auth::id())->latest('updated_at')->paginate(5);
        return view('notes.index')->with('notes',$notes);
     }
 
--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -11,7 +11,7 @@
             <div class="my-6 p-6 bg-white border-b border-gray-200 shadow-sm sm:rounded-lg">
                 <h2 class="font-bold text-2xl">{{$note->title}}</h2>
 
-                <p class="mt-2">{{$note->text}}</p>
+                <p class="mt-2">{{Str::limit($note->text,200)}}</p>
                 <span>{{$note->updated_at->diffForHumans()}}</span>
             </div>
             @empty
